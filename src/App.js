d6d6b0fb68c5ed0c46b71b59c66c154e3d779e53
Kenia Valladarez <keniavalladarez@gmail.com>
2017-12-04 22:51:25 -0500

    Creates toggle to turn state on/off.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: true
+    };
+  }
+  toggle = () => {
+    this.setState({isOn: !this.state.isOn})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome Kenia</h1>
-          <MyComponent name="Kenia"/>
-        </header>
+          <h2>Welcome to React</h2>
+          <MyComponent name="Kenia 😃" />
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.toggle}>
+          {this.state.isOn ? 'On' : 'Off'}
+        </button>
       </div>
     );
   }
