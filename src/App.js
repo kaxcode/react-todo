13b244de213bd889f03bfdc4c28355559f3c2a05
Kenia Valladarez <keniavalladarez@gmail.com>
2017-12-06 22:20:27 -0500

    Setup for simple api.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import List from './components/List';
-
-export default class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       term: '',
-      items: [],
-      clicks: 0,
-      show: true
+      img: ''
     };
   }
-
-  onChange = (event) => {
-    this.setState({ term: event.target.value });
-  }
-
-  onSubmit = (event) => {
-    event.preventDefault();
-    this.setState({
-      term: '',
-      items: [...this.state.items, this.state.term]
-    });
-  }
-
-  IncrementItem = () => {
-    this.setState({ clicks: this.state.clicks + 1 });
-  }
-  DecreaseItem = () => {
-    this.setState({ clicks: this.state.clicks - 1 });
-  }
-  ToggleClick = () => {
-    this.setState({ show: !this.state.show });
-  }
-
-  render() {
-    return (
-      <div>
-        <form className="App" onSubmit={this.onSubmit}>
-          <input value={this.state.term} onChange={this.onChange} />
-          <button>Submit</button>
-        </form>
-        <List items={this.state.items} />
-        <button onClick={this.IncrementItem}>Click to increment by 1</button>
-        <button onClick={this.DecreaseItem}>Click to decrease by 1</button>
-        <button onClick={this.ToggleClick}>
-          { this.state.show ? 'Hide number' : 'Show number' }
-        </button>
-        { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
-      </div>
-    );
-  }
 }
+export default App;
