1cf02143dfeb0bd9292656d5445bc1e6e95b6e51
Kenia Valladarez <keniavalladarez@gmail.com>
2017-12-06 22:31:13 -0500

    creates an api with giphy images.

--- src/App.js
+++ src/App.js
@@ -10,20 +10,30 @@ class App extends Component {
     };
   }
 
-
   onChange = (event) => {
-    this.setState({term: event.target.value});
+    this.setState({ term: event.target.value });
   }
-  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const api_key = 'dc6zaTOxFJmzC';
+    const url = `http://api.giphy.com/v1/gifs/search?q=${this.state.term}&api_key=${api_key}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ term:'', img: data.data[0].images.fixed_height.url }))
+      .catch(e => console.log('error', e));
+  }
 
   render() {
     return (
       <div className="App">
-        <input value={this.state.term} onChange={this.onChange} />
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Search!</button>
+        </form>
+        <img src={this.state.img} height="200" alt={this.state.term} />
       </div>
     );
   }
 }
-
 
 export default App;
