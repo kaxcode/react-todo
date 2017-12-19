345997c2d1f12faa102a0ba988ccf60a29f2ce3f
Kenia Valladarez <keniavalladarez@gmail.com>
2017-12-18 23:06:19 -0500

    Adds todo for testing.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import List from './components/List';
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       term: '',
-      img: ''
+      items: []
     };
   }
 
   onChange = (event) => {
     this.setState({ term: event.target.value });
   }
-  handleSubmit = (event) => {
+
+  onSubmit = (event) => {
     event.preventDefault();
-    const api_key = 'dc6zaTOxFJmzC';
-    const url = `http://api.giphy.com/v1/gifs/search?q=${this.state.term}&api_key=${api_key}`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => this.setState({ term:'', img: data.data[0].images.fixed_height.url }))
-      .catch(e => console.log('error', e));
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
   }
 
   render() {
     return (
-      <div className="App">
-        <form onSubmit={this.handleSubmit}>
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
           <input value={this.state.term} onChange={this.onChange} />
-          <button>Search!</button>
+          <button>Submit</button>
         </form>
-        <img src={this.state.img} height="200" alt={this.state.term} />
+        <List items={this.state.items} />
       </div>
     );
   }
 }
-
-export default App;
