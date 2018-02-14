d46ead926c601bfab450163e20d7d4413e0eb2f4
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-02-14 11:46:00 -0500

    Adds isAuthenticated state to the App.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import './App.css';
 import RouteNavItem from './components/RouteNavItem';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+  
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+  
   render() {
     return (
       <div className="App container">
