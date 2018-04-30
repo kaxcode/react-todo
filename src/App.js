89d9e127f90e2400cb1e5dd8b2cbcf0af5d12206
Kenia <keniavalladarez@gmail.com>
2018-04-30 17:38:27 -0400

    Define a function on the state of the AppProvider context to increment number.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 const AppContext = React.createContext();
 
 class AppProvider extends Component {
   state = {
     number: 10,
+    inc: () => {
+      this.setState({ number: this.state.number + 1 });
+    }
   };
 
   render() {
-    return <AppContext.Provider value={this.state}>{this.props.children}</AppContext.Provider>;
+    return (
+      <AppContext.Provider value={this.state}>
+        {this.props.children}
+      </AppContext.Provider>
+    );
   }
 }
 
@@ -20,6 +27,9 @@ const Green = () => (
 
 const Blue = () => (
   <div className="blue">
+    <AppContext.Consumer>
+      {context => <button onClick={context.inc}>INC</button>}
+    </AppContext.Consumer>
     <Green />
   </div>
 );
@@ -30,7 +40,9 @@ class Red extends Component {
       <AppProvider>
         <div className="App__container">
           <div className="red">
-            <AppContext.Consumer>{context => context.number}</AppContext.Consumer>
+            <AppContext.Consumer>
+              {context => context.number}
+            </AppContext.Consumer>
             <Blue />
           </div>
         </div>
